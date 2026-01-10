ad931d349c79126ba37c82124db6b0abc4cdf9ce
Kerwin Wang <wjw20020209@outlook.com>
2026-01-10 19:06:59 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   },
   // 如果您的仓库名是 'talos-archives' 且没有绑定自定义域名，
   // 请取消下面这行的注释，并将 '/talos-archives' 替换为您实际的仓库名：
-  // basePath: '/talos-archives',
+  basePath: '/talos-archives',
 };
 
 export default nextConfig;
